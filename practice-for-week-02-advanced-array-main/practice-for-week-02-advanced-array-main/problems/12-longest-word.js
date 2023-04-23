023f8fecb3ccb6ca067de55da6606537b6650c12
HannahGuertler <guertler99@gmail.com>
2023-04-23 18:59:42 -0400

    solved longestWord

--- practice-for-week-02-advanced-array-main/practice-for-week-02-advanced-array-main/problems/12-longest-word.js
+++ practice-for-week-02-advanced-array-main/practice-for-week-02-advanced-array-main/problems/12-longest-word.js
@@ -18,6 +18,35 @@ console.log(longestWord('')); // ''
 
 
 // Your code here
+const longestWord = function(sentence) {
+    // if the string is empty, return the string
+    if (sentence === "") return sentence;
+
+    // make a var to represent the longest word so far, initialized to an empty string
+    let longWord = "";
+    // make a var to represent the longest word length so far, initialized to 0
+    let longWordLength = 0;
+
+    // .split the str into an array on spaces
+    let wordArr = sentence.split(" ");
+
+    // iterate through the array using .reduce
+    // set up this function with acc, word.
+    //  acc initialized to 0
+    wordArr.reduce((acc, word) => {
+      // if the current word's length is longer than the longest word so far,
+      if (word.length > longWordLength) {
+
+        //  set the current word to be the longest word
+        longWord = word;
+        //  set the longest word length to the length of the current word
+        longWordLength = word.length;
+      }
+      return longWord;
+    }, 0)
+    return longWord;
+  }
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
